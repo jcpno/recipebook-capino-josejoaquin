29d083abf047fcf83ae7f0201b00cb4f440d8b92
jcpno <102474586+jcpno@users.noreply.github.com>
2025-02-13 23:30:42 +0800

    Add Recipe 1 and 2 pages

--- recipebook/ledger/urls.py
+++ recipebook/ledger/urls.py
@@ -4,8 +4,8 @@ from . import views
 
 urlpatterns = [
     path("recipes/list", views.recipes_list, name="recipes-list"),
-    #path("/recipe/1", views.index),
-    #path("/recipe/2", views.index)
+    path("recipe/1", views.recipe_1),
+    path("recipe/2", views.recipe_2)
 ]
 
 app_name = "ledger"
