dcff6ce357c791cabc527a7ef38bb0811d59740d
jcpno <102474586+jcpno@users.noreply.github.com>
2025-02-27 16:21:47 +0800

    Set string rep and abs url for ingredient model

--- recipebook/ledger/models.py
+++ recipebook/ledger/models.py
@@ -1,8 +1,14 @@
 from django.db import models
-
+from django.urls import reverse
 
 class Ingredient(models.Model):
     name = models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.name
+    
+    def get_absolute_url(self):
+        return reverse('ingredient_detail', args=[str(self.name)])
 
 
 class Recipe(models.Model):
