f05c86362e0e87636586a5e88712b3611bcadcbb
jcpno <102474586+jcpno@users.noreply.github.com>
2025-02-27 15:21:47 +0800

    Create recipe model

--- recipebook/ledger/models.py
+++ recipebook/ledger/models.py
@@ -3,3 +3,7 @@ from django.db import models
 
 class Ingredient(models.Model):
     name = models.CharField(max_length=100)
+
+
+class Recipe(models.Model):
+    name = models.CharField(max_length=100)
