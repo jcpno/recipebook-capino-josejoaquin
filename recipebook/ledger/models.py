f1434c9225b3e2692637336c5d9ddff4b35b70b5
jcpno <102474586+jcpno@users.noreply.github.com>
2025-02-27 15:48:18 +0800

    Create RecipeIngredient model

--- recipebook/ledger/models.py
+++ recipebook/ledger/models.py
@@ -7,3 +7,17 @@ class Ingredient(models.Model):
 
 class Recipe(models.Model):
     name = models.CharField(max_length=100)
+
+
+class RecipeIngredient(models.Model):
+    qty = models.IntegerField()
+    ingredient = models.ForeignKey(
+        Ingredient,
+        on_delete=models.PROTECT,
+        related_name='recipe'
+    )
+    recipe = models.ForeignKey(
+        Recipe,
+        on_delete=models.PROTECT,
+        related_name='ingredients'
+    )
